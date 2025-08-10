3cab6102e08709f367e82c5ca24a8b0eaafd5a8b
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-08-10 17:13:22 +0300

    style(frontend): add background image, semi-transparent card backgrounds, and hover scale effect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import CharactersList from "./components/CharactersList.jsx";
+import * as url from "node:url";
 
 function App() {
 
   return (
-    <>
+    <div style={{backgroundImage: "url('/mcu-background.jpg')"}} className="bg-cover">
       <h1>Welcome to the MCU characters database</h1>
       <CharactersList/>
-    </>
+    </div>
   )
 }
 
--- frontend/src/components/CharactersList.jsx
+++ frontend/src/components/CharactersList.jsx
@@ -20,16 +20,23 @@ export default function CharactersList() {
     return (
         <div className="flex gap-10 flex-wrap justify-center">
             {characters.map((character, index) => (
-                <div key={index} className="w-1/4 bg-gray-300 rounded-lg p-5 flex justify-between">
+                <div key={index} className="w-1/4 rounded-lg p-5 flex justify-between hover:scale-105 duration-500"
+                     style={{ backgroundColor: 'rgba(156, 163, 175, 0.8)' }}>
                     <div className="flex flex-col justify-center gap-2">
-                        <p className="text-red-600 font-bold">{character.id}</p>
+                        <p className="text-rose-900 font-bold">{character.id}</p>
                         <h1 className="text-xl font-bold">{character.name}</h1>
                         <p>{character.realName}</p>
-                        <p className="text-blue-600">{character.universe}</p>
+                        <p className="text-cyan-950">{character.universe}</p>
                     </div>
                     <div className="flex flex-col justify-center p-5 items-center gap-5">
-                        <button>Update</button>
-                        <button>Delete</button>
+                        <button className="border rounded-lg p-2 w-20 hover:bg-cyan-950
+                        hover:text-white cursor-pointer duration-500">
+                            Update
+                        </button>
+                        <button className="border rounded-lg p-2 w-20 hover:bg-rose-900
+                        hover:text-white cursor-pointer duration-500">
+                            Delete
+                        </button>
                     </div>
                 </div>
             ))}
