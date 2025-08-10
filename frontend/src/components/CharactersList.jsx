d1be6d39b6da44cd623253a7d65ef8771dcbafff
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-08-10 16:36:34 +0300

    feat(frontend): fetch MCU characters on mount with axios in CharactersList

--- frontend/src/components/CharactersList.jsx
+++ frontend/src/components/CharactersList.jsx
@@ -1,5 +1,20 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-URL = "http://localhost:3000";
+const URL = "http://localhost:3000";
 
+export default function CharactersList() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${URL}/characters`)
+            .then((response) => {
+                const charactersList = response.data.characters;
+                setCharacters(charactersList);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+}
