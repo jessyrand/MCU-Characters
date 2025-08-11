2fc353a8a53f2df4dbf475c3d03094431fd6aaa2
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-08-11 10:23:57 +0300

    feat(frontend): add Footer Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
 import CharactersList from "./components/CharactersList.jsx";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
     <div style={{backgroundImage: "url('/mcu-background.jpg')"}} className="bg-cover bg-fixed flex flex-col gap-5">
       <Header/>
       <CharactersList/>
+      <Footer/>
     </div>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="flex items-center justify-center bg-cover gap-5 p-1"
+                style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+            <p className="text-white font-light text-sm">Jessy - STD24004</p>
+        </footer>
+    )
+}
